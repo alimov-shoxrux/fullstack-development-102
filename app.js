d17f0abb320f0c7f3d618e15543b563756c6d88f
shaalimov923 <shaalimov923@icloud.com>
2026-02-27 14:25:18 +0500

    homework

--- app.js
+++ app.js
@@ -1,25 +1,81 @@
-// var a = undefined;
+//Task 1
+function countEvenOdd(n) {
+    let evenCount = 0;
+    let oddCount = 0;
 
-// console.log(a);
-// let a = 20;
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 === 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
 
-// greet();
-
-// function greet() {
-//   console.log("hello");
-// }
-
-// console.log(Array())
-
-// Array.prototype.greet = function () {
-//   console.log("Arrayy prototypedan keyapdi");
-// };
-
-// Array().greet();
-
-
-function add () {
-    console.log("hello");
+    console.log("Even numbers:", evenCount);
+    console.log("Odd numbers:", oddCount);
 }
+countEvenOdd(10);
 
-// salom
+//Tassk 2
+
+function reverseNumber(num) {
+    let reversed = 0;
+
+    while (num > 0) {
+        let digit = num % 10;       
+        reversed = reversed * 10 + digit; 
+        num = Math.floor(num / 10); 
+    }
+
+    console.log("Reversed number:", reversed);
+    return reversed;
+}
+reverseNumber(1234);
+
+//Tsk 3
+function largestDigit(num) {
+    let largest = 0;
+
+    while (num > 0) {
+        let digit = num % 10;       
+        if (digit > largest) {
+            largest = digit;
+        }
+        num = Math.floor(num / 10); 
+    }
+
+    console.log("Largest digit:", largest);
+    return largest;
+}
+largestDigit(58392);
+
+//TAsk 4
+
+function sumOfDigits(num) {
+    let sum = 0;
+
+    while (num > 0) {
+        let digit = num % 10;       
+        sum += digit;               
+        num = Math.floor(num / 10);
+    }
+
+    console.log("Raqamlar yig‘indisi:", sum);
+    return sum;
+}
+sumOfDigits(1234);
+
+//Task 5
+
+function countDigits(num) {
+    let count = 0;
+
+    while (num > 0) {
+        num = Math.floor(num / 10); 
+        count++;                    
+    }
+
+    console.log("Raqamlar soni:", count);
+    return count;
+}
+countDigits(12345);
